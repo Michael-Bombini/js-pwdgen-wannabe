9d68883a0354aa4c88917aa6d249ef24fc4afa8e
Michael-Bombini <bombini.michael@gmail.com>
2022-05-18 10:08:57 +0200

    Changed year to 22 instead of 2022

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ Infine scrivi sulla pagina nomecognomecolorepreferito22
 const nome = prompt("Inserisci il tuo nome");
 const cognome = prompt("inserisci il tuo cognome");
 const colorePreferito = prompt("inserisci il tuo colore preferito");
-let anno = 2022 ; 
+let anno = 22 ; 
 
 const pwGenerated = document.getElementById("password");
 pwGenerated.innerHTML = ` <h1>La tua password generata è : </h1> <br>
